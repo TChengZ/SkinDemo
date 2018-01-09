1e90f5613f47fb3e5d7d922c0f2d6ae032bfa1ab
扇羽 <zujie.czj@autonavi.com>
2018-01-09 21:15:30 +0800

    资源整理，新增用于制作皮肤资源的工程

--- app/src/main/java/com/jc/skindemo/SkinActivity.java
+++ app/src/main/java/com/jc/skindemo/SkinActivity.java
@@ -1,13 +1,22 @@
 package com.jc.skindemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-public class SkinActivity extends AppCompatActivity {
+public class SkinActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skin);
+    }
+
+    public void skinChange(View view){
+
+    }
+
+    public void skinDefault(View view){
+
     }
 }
---
+++ skinmoudle/one/src/main/java/com/jc/skindemo/SkinActivity.java
@@ -0,0 +1,7 @@
+package com.jc.skindemo;
+
+import android.support.v4.app.FragmentActivity;
+
+public class SkinActivity extends FragmentActivity {
+
+}
